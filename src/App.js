d1fa786c1eb8a945cf5c3190f9befd8dc4eb29a9
Van DeSantis <vandesantis98@gmail.com>
2019-03-28 21:42:23 -0500

    Add Navigation bar with sign in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navigation />
         <h1>TV Show List</h1>
       </div>
     );
